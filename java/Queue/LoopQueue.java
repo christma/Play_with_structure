9167b4a6ac3dab1a538c2b69aa5e655d1fd425ea
xianliangliu <xianliangliu@aliyun.com>
2019-03-31 23:59:31 +0800

    Loop Queue

---
+++ java/Queue/LoopQueue.java
@@ -0,0 +1,99 @@
+package Queue;
+
+public class LoopQueue<E> implements Queue<E> {
+
+    private E[] data;
+    private int front, tail;
+    private int size;
+
+    public LoopQueue(int capasity) {
+        data = (E[]) new Object[capasity + 1];
+        front = 0;
+        tail = 0;
+        size = 0;
+    }
+
+
+    public LoopQueue() {
+        this(10);
+    }
+
+    public int getCapacity() {
+        return data.length - 1;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return front == tail;
+    }
+
+    @Override
+    public int getSize() {
+        return size;
+    }
+
+
+    @Override
+    public void enqueue(E e) {
+        if ((tail + 1) % data.length == front)
+            resize(getCapacity() * 2);
+
+        data[tail] = e;
+        tail = (tail + 1) % data.length;
+        size++;
+    }
+
+    @Override
+    public E dequeue() {
+        if (isEmpty())
+            throw new IllegalArgumentException("Cannot dequeue from an empty queue");
+        E ret = data[front];
+        data[front] = null;
+        front = (front + 1) % data.length;
+        size--;
+        if (size == getCapacity() / 4 && getCapacity() / 2 != 0)
+            resize(getCapacity() / 2);
+
+        return ret;
+    }
+
+    @Override
+    public E getFront() {
+        if (isEmpty())
+            throw new IllegalArgumentException("Cannot dequeue from an empty queue");
+        return data[front];
+    }
+
+    private void resize(int newcapacity) {
+        E[] newData = (E[]) new Object[newcapacity + 1];
+        for (int i = 0; i < size; i++) {
+            newData[i] = data[(i + front) % data.length];
+
+            data = newData;
+            front = 0;
+            tail = size;
+        }
+
+    }
+
+
+    @Override
+    public String toString() {
+        StringBuilder res = new StringBuilder();
+
+
+        res.append(String.format(" Queue : size = %d, capacity = %d \n", size, getCapacity()));
+
+        res.append("front [ ");
+        for (int i = 0; i < size; i++) {
+            res.append(data[i]);
+            if (i != size - 1)
+                res.append(", ");
+        }
+        res.append(" ]  tail");
+        return res.toString();
+    }
+
+
+
+}
